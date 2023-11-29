0a33ee5c87c81904875a1b3fcffb9ae3f37eba39
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-11-29 16:43:03 +1000

    database.jsc Chagned

--- database.js
+++ database.js
@@ -1,14 +1,8 @@
-import mysql from "mysql2/promise";
-
-// Ensure these environment variables are set in your deployment environment
-const host = process.env.DB_HOST || "localhost";
-const user = process.env.DB_USER || "mobile-hour-app";
-const password = process.env.DB_PASSWORD || "Sanjay2001!";
-const database = process.env.DB_DATABASE || "mobile-hour-2023";
+import mysql from "mysql2/promise"
 
 export const db_conn = mysql.createPool({
-    host,
-    user,
-    password,
-    database
-});
+    host: "localhost",
+    user: "mobile-hour-app",
+    password : "Sanjay2001!",
+    database: "mobile-hour-2023"
+})
