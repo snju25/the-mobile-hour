93dd5d767de6fa47433f4800a860d3190b84fc8f
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-11-29 16:56:55 +1000

    final_Changes

--- database.js
+++ database.js
@@ -1,8 +1,11 @@
-import mysql from "mysql2/promise"
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db_conn = mysql.createPool({
-    host: "localhost",
-    user: "mobile-hour-app",
-    password : "Sanjay2001!",
-    database: "mobile-hour-2023"
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
